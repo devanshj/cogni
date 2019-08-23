05484f758b73891b191fb258519732422bcc0959
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2019-08-23 19:39:23 +0530

    Bug fixes, bump staerm

--- src/ui/state/staerm-state.ts
+++ src/ui/state/staerm-state.ts
@@ -2,53 +2,78 @@ import { Cogni } from "../../core";
 import { toNavigation, toFocusedAreaIndex } from "./helpers";
 
 import { Observable, merge } from "rxjs";
-import { withLatestFrom, map, scan } from "rxjs/operators";
+import { withLatestFrom, map, scan, filter } from "rxjs/operators";
 import { KeypressData, TerminalState, r as staermR } from "staerm";
+import { use, notNull } from "../../utils";
 
 
 export const toStaermState = (
 	cogniOutput$: Observable<Cogni.Output>,
 	keypress$: Observable<KeypressData>
 ) => 
-	merge(
+	use(
 		keypress$.pipe(
 			toNavigation(),
 			toFocusedAreaIndex(
 				cogniOutput$.pipe(map(o => o.stdinAreas.length))
-			),
-			withLatestFrom(cogniOutput$.pipe(map(o => o.stdinAreas))),
-			map(([i, as]): Reducer =>
-				state => 
-					({
-						...state,
-						input: 
-							i === null
-								? null
-								: {
-									...as[i],
-									caretOffset: as[i].length
-								}
-					})
-			)
-		),
-		cogniOutput$.pipe(
-			map((o): Reducer =>
-				state => ({
-					...state,
-					text: o.stdoutText
-				})
-			)
-		),
-		keypress$.pipe(
-			map((key): Reducer => 
-				state => staermR.typing(state, key)
 			)
 		)
-	).pipe(
-		scan(
-			(state, reducer) => reducer(state),
-			{ text: "", input: null } as TerminalState
+	).as(focusedAreaIndex$ =>
+		merge(
+			focusedAreaIndex$.pipe(
+				withLatestFrom(cogniOutput$.pipe(map(o => o.stdinAreas))),
+				map(([i, as]): Reducer =>
+					state => 
+						({
+							...state,
+							input: 
+								i === null
+									? null
+									: {
+										...as[i],
+										caretOffset: as[i].length
+									}
+						})
+				)
+			),
+			cogniOutput$.pipe(
+				withLatestFrom(
+					focusedAreaIndex$.pipe(
+						filter(notNull)
+					)
+				),
+				map(([o, i]): Reducer =>
+					state => ({
+						input: use(o.stdinAreas[
+							i % o.stdinAreas.length
+						]).as(area =>
+							area
+								? {
+									...area,
+									caretOffset:
+										state.input
+											? state.input.caretOffset
+											: 0
+								}
+								: null
+						),
+						text: o.stdoutText
+					})
+				)
+			),
+			keypress$.pipe(
+				map((key): Reducer => 
+					state => staermR.typing(state, key)
+				)
+			)
+		).pipe(
+			scan(
+				(state, reducer) => reducer(state),
+				{ text: "", input: null } as TerminalState
+			)
 		)
 	);
+
+const cyclicAdd = (x: number, m: number) => (x + m) % m;
 
 type Reducer = (state: TerminalState) => TerminalState;
