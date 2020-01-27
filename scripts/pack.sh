d25b6f8481c9afc2a59f030d2c7a9d0ea6b91bec
Devansh Jethmalani <30295578+devanshj@users.noreply.github.com>
2020-01-27 15:30:18 +0530

    fix(scripts/pack.sh): remove node target version
    Try removing node target version from nexe to use node version from environment

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -3,5 +3,5 @@ version="$(
 	sed 's/\s*\"version\":\s*\"\(.*\)\"\s*,\s*/\1/'
 )"
 for target in windows-x64 windows-x86 linux-x64 linux-x86 macos-x64; do
-    ./node_modules/.bin/nexe bundled/index.js -t $target-10.16.0 -o build/cogni-$version-$target
+    ./node_modules/.bin/nexe bundled/index.js -t $target -o build/cogni-$version-$target
 done
